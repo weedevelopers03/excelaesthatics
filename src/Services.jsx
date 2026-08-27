0645d79d8768d2bdcc7f114302ec4205d603ba7a
WeeDevelopers <ismaanwar.ia@gmail.com>
2026-08-27 11:45:58 +0500

    services update

--- src/Services.jsx
+++ src/Services.jsx
@@ -3,15 +3,9 @@ import { useState } from 'react'
 const Services = () => {
   const [visibleServices, setVisibleServices] = useState(6)
 
+ 
   const allServices = [
     // Consults
-    {
-      name: 'Weightloss Packages',
-      price: 'Price varies',
-      duration: '30 mins+',
-      description:
-        'Indulge in our exclusive Weightloss Packages tailored to meet your unique needs. Experience the benefits of mesotherapy injections, wood therapy, and velashape treatments, all designed to help you achieve your wellness goals. Book your appointment today and embark on a journey towards a healthier and happier you!',
-    },
     {
       name: 'Virtual Consultation',
       price: 'Price varies',
@@ -19,7 +13,7 @@ const Services = () => {
       description:
         'Unlock the power of personalized advice with our virtual consult service! Say goodbye to long waiting times and hello to convenient, time-saving solutions tailored just for you.',
     },
-
+ 
     // Weight loss and injections
     {
       name: 'Ultrasound Lymphatic Massages',
@@ -42,7 +36,7 @@ const Services = () => {
       description:
         'Looking to achieve a more youthful appearance? Book an appointment for our PDO Threads service, where our skilled professionals will use Mint PDO threads to help tighten and lift your skin, giving you a refreshed and rejuvenated look. Don not wait any longer, schedule your appointment today!',
     },
-
+ 
     // Neurotoxin and fillers
     {
       name: 'Skinny Shot',
@@ -50,13 +44,6 @@ const Services = () => {
       duration: '15 mins',
       description:
         'Boost your fat loss with our Skinny Shot! This powerful shot accelerates fat metabolism, converts carbs and sugars into energy, and speed up muscle recovery, making it perfect for enhancing your workouts and seeing quicker results. Get leaner and more energized with every session.',
-    },
-    {
-      name: 'Sermorelin Therapy',
-      price: 'Price varies',
-      duration: '30 mins',
-      description:
-        'Stimulate your body’s natural growth hormone production with sermorelin, a powerful peptide therapy designed to support deeper sleep, improved metabolism, fat loss, muscle recovery, and overall vitality. start your journey to balance hormones and healthy lifestyle today.',
     },
     {
       name: 'Semaglutide Injections',
@@ -72,7 +59,7 @@ const Services = () => {
       description:
         'Experience our professional Salmon DNA/PDRN treatment, designed to promote skin regeneration and improve overall skin texture. Utilizing PDRN derived from salmon DNA, this service supports cellular repair and enhances hydration for a revitalized appearance. Book your one-on-one appointment today for a tailored session that prioritizes your skincare needs.',
     },
-
+ 
     // Skin and rejuvenation
     {
       name: 'Reta Triple G Weight Loss Program',
@@ -101,13 +88,6 @@ const Services = () => {
       duration: '30 mins',
       description:
         'Peptide Therapy is designed to enhance recovery, support wellness and optimize body goals. These treatments use targeted peptides (short chains of amino acids) that work at a cellular level to improve energy, support tissue and joint repair, boost metabolism, reduce inflammation, stimulate natural growth hormone, and promote anti-aging benefits. Each peptide has specific functions, such as improving sleep quality, aiding fat loss, enhancing muscle development, accelerating healing, and rejuvenating skin.',
-    },
-    {
-      name: 'NAD+ Therapy',
-      price: 'Price varies',
-      duration: '30 mins',
-      description:
-        'NAD and NAD+ are naturally occurring compounds in the body that play a key role in energy generation. NAD+ is especially important as it acts as a critical co-factor to enhance mitochondrial function. Mitochondria, often called the cell’s "energy powerhouses," convert micronutrients into ATP, the energy currency of our cells. As we age, NAD levels naturally decline due to reduced production and increased oxidative stress from environmental factors. This decline in NAD+ is linked to symptoms like fatigue, mental fog, dull skin, and poor sleep. Boosting NAD+ levels may support a broad range of health issues, from metabolic conditions like diabetes to more complex diseases like cancer.',
     },
     {
       name: 'Microneedling',
@@ -145,13 +125,6 @@ const Services = () => {
         'Letybo is a Korean-made, FDA-approved botulinum toxin treatment used to reduce the appearance of dynamic facial wrinkles, such as frown lines, forehead lines, and crow feet. Quick, minimally invasive, and effective, Letybo helps you achieve a smoother, more youthful look with natural results.',
     },
     {
-      name: 'LED Light Therapy',
-      price: 'Price varies',
-      duration: '15–30 mins',
-      description:
-        'LED (light-emitting diode) light therapy is a non-invasive treatment that enters the skin’s layers to improve the skin. Red LED light therapy may reduce inflammation and stimulate the production of collagen, a protein responsible for younger-looking skin that diminishes with age, treatment for wrinkles Blue LED light therapy may destroy acne-causing bacteria (P. acnes). Yellow LED light for skin erythema, relieves redness',
-    },
-    {
       name: 'L-Carnite Shots',
       price: 'Price varies',
       duration: '30 mins',
@@ -172,7 +145,7 @@ const Services = () => {
       description:
         'Initial consult to discuss your needs and create a customized package. Consult pricing will be discounted from the service.',
     },
-
+ 
     // Laser and devices
     {
       name: 'Hylanex/ Filler Reversal',
@@ -209,7 +182,7 @@ const Services = () => {
       description:
         'Fillers are a cosmetic injection the team at Excel Aesthetics use to enhance your beauty by improving facial contours and diminishing the appearance of lines and wrinkles. The fillers contain a gel-like substance that adds volume under your skin to help you achieve specific aesthetic goals. The team at Excel Aesthetics may recommend fillers to: Soften lines and creases Plump up thin lips Improve cheeks contours Fill in recessed scars Fix facial deformities Your specialist may also use fillers to add volume under your lids to remove the shadows that make you look tired and worn out. Consultation Required. Pricing based on 1ML. Block time includes numbing time.',
     },
-
+ 
     // Body and contouring
     {
       name: 'Facial Balancing',
@@ -239,7 +212,7 @@ const Services = () => {
       description:
         'Biorepeel Rose is specifically designed to lighten pigmentation in intimate areas such as underarms, bikini, inner thighs and buttocks.',
     },
-
+ 
     // Brows, lashes, lips
     {
       name: 'Biorepeel Gold',
@@ -261,6 +234,219 @@ const Services = () => {
       duration: '10 mins+',
       description:
         'Revitalize your body and mind with our B12 Shot service. Feel a surge of energy as it boosts your natural vitality, while supporting brain health and improving your mood. ',
+    },
+ 
+    // Laser & removal treatments
+    {
+      name: 'Three For Me',
+      price: 'US$50.00+',
+      duration: '30 mins',
+      description:
+        'ThreeForMe is a laser treatment that addresses multiple key skin concerns—wrinkles, sun damage, and uneven tone—in a single, efficient session for smoother, more radiant skin.',
+    },
+    {
+      name: 'Tattoo Removal',
+      price: '$100–$300',
+      duration: '45 mins',
+      description:
+        'Our professional laser tattoo removal service offers a safe and effective process tailored to your needs, using advanced laser technology to gradually and safely fade unwanted ink.',
+    },
+    {
+      name: 'Spider Vein Removal',
+      price: 'US$65.00+',
+      duration: '15 mins',
+      description:
+        'Sclerotherapy is a non-invasive medical procedure designed to effectively treat varicose and spider veins, enhancing both the look and health of your legs.',
+    },
+    {
+      name: 'Nail Fungal Removal',
+      price: 'US$200.00',
+      duration: '45 mins',
+      description:
+        'Our laser nail fungal removal service offers an effective and non-invasive solution for treating fungal infections, utilizing advanced laser technology to target fungus at the source.',
+    },
+    {
+      name: 'Skin Tag Removal',
+      price: 'US$100.00',
+      duration: '40 mins',
+      description:
+        'Say goodbye to skin tags with our effective and painless removal service. Book an appointment to discuss your treatment options today.',
+    },
+    {
+      name: 'Laser Hair Removal',
+      price: 'Pricing based on 6 sessions',
+      duration: '10 mins+',
+      description:
+        'Laser hair removal is a permanent solution to unwanted hair. The team at Excel Aesthetics uses advanced laser technology for smooth, long-lasting results.',
+    },
+    {
+      name: 'Laser IPL/Fractional',
+      price: 'Price varies',
+      duration: '30 mins+',
+      description:
+        'The Icon aesthetic system is a versatile beauty tool equipped with nonablative fractional technology, ideal for improving skin tone, texture, and pigmentation.',
+    },
+ 
+    // Body treatments
+    {
+      name: 'Massage Therapy',
+      price: 'US$55.00+',
+      duration: '30 mins',
+      description:
+        'Relax, restore and reset. Our professional massage services are designed to relieve tension, ease stress, and leave you feeling renewed.',
+    },
+    {
+      name: 'Lymphatic Drainage Massage',
+      price: 'US$110.00+',
+      duration: '30 mins',
+      description:
+        'Lymphatic Drainage Massage uses gentle, rhythmic techniques to encourage lymph flow, reduce fluid retention, and support your body’s natural detoxification process.',
+    },
+    {
+      name: 'Wood Therapy',
+      price: 'US$100.00+',
+      duration: '45 mins',
+      description:
+        'Embark on a journey of natural healing and body sculpting with our exclusive Wood Therapy, using traditional wooden tools to help contour and tone the body.',
+    },
+    {
+      name: 'Body Contouring',
+      price: 'US$99.00+',
+      duration: '30 mins',
+      description:
+        'Achieve a more sculpted, toned appearance with our non-invasive body contouring treatments, tailored to help you reach your aesthetic goals.',
+    },
+    {
+      name: 'Roller Wave Treatment/Endosphere',
+      price: 'US$100.00+',
+      duration: '30 mins',
+      description:
+        'Smooth, sculpt and define with our advanced Roller Wave Treatment! This non-invasive compression micro-vibration therapy improves circulation and skin texture.',
+    },
+    {
+      name: 'Skin Tightening/Body Contouring',
+      price: 'Price varies per section',
+      duration: '15 mins+',
+      description:
+        'TempSure™ Envi uses radiofrequency energy to gently heat tissue beneath the skin, stimulating collagen production for firmer, tighter-looking skin.',
+    },
+    {
+      name: 'Vaginal Rejuvenation',
+      price: 'US$400.00+',
+      duration: '30 mins',
+      description:
+        'The TempSure Vitalia treatment is a non-invasive, non-hormonal and chemical-free solution for feminine wellness, using radiofrequency energy for comfort and confidence.',
+    },
+    {
+      name: 'Intimate Whitening Treatments',
+      price: 'Price varies',
+      duration: '30 mins',
+      description:
+        'Experience the confidence-boosting results of our professional-grade intimate whitening treatments, designed for gentle and effective brightening.',
+    },
+    {
+      name: 'Carbon Peel',
+      price: 'US$200.00',
+      duration: '30 mins',
+      description:
+        'Experience the revitalizing effects of a Carbon Peel, designed to deeply cleanse and rejuvenate your skin while helping minimize the appearance of pores.',
+    },
+ 
+    // Injectables & skin boosters
+    {
+      name: 'Jeuveau',
+      price: 'US$12.00',
+      duration: '30 mins',
+      description:
+        'Jeuveau appointments provide a professional solution for reducing the appearance of wrinkles, delivering natural-looking results with minimal downtime.',
+    },
+    {
+      name: 'Daxxify',
+      price: 'US$12.00',
+      duration: '30 mins',
+      description:
+        'Daxxify is a peptide-formulated injectable treatment designed to help reduce the appearance of wrinkles and fine lines, with results that last longer than traditional neurotoxins.',
+    },
+    {
+      name: 'Dysport',
+      price: 'US$7.00',
+      duration: '30 mins',
+      description:
+        'Dysport is a fast-acting injectable treatment that smooths moderate to severe frown lines for a naturally refreshed appearance.',
+    },
+    {
+      name: 'Skin Boosters',
+      price: 'US$199.00+',
+      duration: '30 mins',
+      description:
+        'Skin Boosters deliver deep hydration beneath the surface, improving elasticity, texture, and overall radiance for a healthy, dewy glow.',
+    },
+    {
+      name: 'Dark Circle & Under Eye Rejuvenation (Toskani)',
+      price: 'US$250.00+',
+      duration: '30 mins',
+      description:
+        'Our Undereye Contour service is designed to brighten, smooth, and reduce puffiness around the delicate eye area, leaving you looking refreshed and well-rested.',
+    },
+ 
+    // Lash, brow & beauty
+    {
+      name: 'Lash Extensions',
+      price: 'US$110.00+',
+      duration: '2 hrs',
+      description:
+        'We specialize in enhancing your natural beauty with our expertly applied eyelash extensions. Say goodbye to daily mascara and hello to effortless, full lashes.',
+    },
+    {
+      name: 'Lip Blushing',
+      price: 'US$50.00+',
+      duration: '30 mins',
+      description:
+        'Lip Blushing is a type of semi-permanent cosmetic tattooing procedure achieved by depositing pigment into the lips for a soft, natural tint and defined shape.',
+    },
+    {
+      name: 'Korean Lash Lift & Tint',
+      price: 'US$130.00',
+      duration: '2 hrs',
+      description:
+        'The next generation of lash lifting. Our Korean Lash Lift & Tint delivers a beautifully lifted, natural curl with rich, long-lasting color.',
+    },
+    {
+      name: 'Brows',
+      price: 'US$45.00+',
+      duration: '30 mins',
+      description:
+        'Get ready to elevate your look with our expert brow shaping service! Achieve a natural, flawless brow that frames and enhances your features.',
+    },
+    {
+      name: 'Powder Brows',
+      price: 'Price varies',
+      duration: '1 hr 45 mins+',
+      description:
+        'Powder Brows is a cosmetic semi-permanent makeup technique that creates a soft, powdered brow effect for fuller, more defined arches.',
+    },
+    {
+      name: 'Facials',
+      price: 'US$88.00+',
+      duration: '1 hr 10 mins+',
+      description:
+        'Indulge in a rejuvenating facial experience that will leave you feeling refreshed and glowing, customized to your skin’s unique needs.',
+    },
+ 
+    // Promotions & packages
+    {
+      name: 'PDO Thread Promotion',
+      price: 'US$1,500.00',
+      duration: '40 mins',
+      description:
+        'Full face PDO Threads.',
+    },
+    {
+      name: 'Event Exclusive Specials',
+      price: 'US$450.00+',
+      duration: '30 mins',
+      description:
+        'Exclusive event-only pricing available for 72 hours after the event. Take advantage of limited-time savings on select treatments.',
     },
   ]
 
